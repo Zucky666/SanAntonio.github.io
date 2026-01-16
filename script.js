8cbb9995a7de1ddf651b42531861b0827e742cab
Zucky666 <yammil2015@hotmail.com>
2026-01-16 13:58:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,12 +19,21 @@ const alumnos = [
     { nombre: "Roberto Duran Mopi", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
     { nombre: "Damaris Yasmin Rivas San Millan", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
     { nombre: "Kevin Cujuy Arias", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
-    { nombre: "Eliana Noy Sucubono", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Eliana Moy Sucubono", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
     { nombre: "Luis Ruiz Pimental", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
     { nombre: "Alexandra Teran Herrera", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
     { nombre: "Raul Suarez Zeballos", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
     { nombre: "Edelberto Mosua Justiniano", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
-    { nombre: "Lizbeth Maza Semo", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" }
+    { nombre: "Yngrid Jimena Romero Vargas", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Henry Raul Hoyos Barboza", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Jose Carlos Quinteros Franco", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Yhon Robert Pinto Navarro", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Jose Henry Yuco Mae", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Elva E. Velarde Vaca", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Ayrton Julio Montero Mancilla", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Wilmer A. Claros Mareca", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Kenia L. Yuco Taborga", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" },
+    { nombre: "Kelly Fabiola Zambrana Alpire", fecha: "00/00/1997", salida: 2014, foto: "./img/alumnos2014/incognito.png" }
     // ... hasta los 34 alumnos
 ];
 
@@ -42,7 +51,10 @@ function renderAlumnos(lista) {
             <img src="${alumno.foto}" class="foto-alumno">
             <div class="info-alumno">
                 <h3>${alumno.nombre}</h3>
-                <p>Fecha de nacimiento: ${alumno.fecha}</p>
+                <p class="fecha-nac">
+                <img src="./img/icono/Pastel.png" class="icono-cumple" alt="Cumpleaños">
+                <span>${alumno.fecha}</span>
+                </p>
                 <p>Año de salida: ${alumno.salida}</p>
             </div>
         `;
@@ -77,6 +89,5 @@ buscador.addEventListener("input", () => {
 
     renderAlumnos(ordenados);
 });
-
 
 
